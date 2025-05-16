a31e02a5676d60011ff83bc457696016610a31a7
Vanmathi Kumar <vanmathikevin@gmail.com>
2025-05-16 17:10:01 +0100

    Added new project for Application Layer Added DTOs for create and retrieve coffee bean objects

---
+++ AllTheBeans.Application/DTOs/CoffeeBeanBaseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AllTheBeans.Application.DTOs
+{
+    public class CoffeeBeanBaseDto
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
