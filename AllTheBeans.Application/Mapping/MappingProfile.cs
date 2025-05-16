e5d90f3cca94767f336cfb07252bf1ee14959345
Vanmathi Kumar <vanmathikevin@gmail.com>
2025-05-16 17:17:15 +0100

    Added Automapper dependency and mapping profile for mapping DTOs to models.

---
+++ AllTheBeans.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿
+using AllTheBeans.Application.DTOs;
+using AllTheBeans.Domain.Models;
+using AutoMapper;
+
+namespace AllTheBeans.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CoffeeBean, CoffeeBeanDto>().ReverseMap();
+            CreateMap<CreateCoffeeBeanDto, CoffeeBean>();
+
+        }
+    }
+}
